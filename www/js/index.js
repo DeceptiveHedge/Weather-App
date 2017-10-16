d3244e88be1693b06e6b7f48b92bce6be20751e0
DeceptiveHedge <monj1_14@uni.worc.ac.uk>
2017-10-16 14:32:56 +0100

    optimization

--- www/js/index.js
+++ www/js/index.js
@@ -2,80 +2,80 @@ var paused_count = 0;
 var resumed_count = 0;
 var launched_count = 0;
 
-    function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-		console.log("device ready");
-    }
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    console.log("device ready");
+}
 	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
+function updateDisplay() {
+    $("#launched").text("Application launched: " + launched_count);
+    $("#resumed").text("Application paused: " + paused_count);
+    $("#paused").text("Application resumed: " + resumed_count);
+}
 
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
+// device APIs are available
+//
+function onDeviceReady() {
 		
-        alert("ready to go");
+    alert("ready to go");
         
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+    document.addEventListener("pause", onPause, false);
 		
-		console.log(device.cordova);
+    console.log(device.cordova);
 		
-		launched_count++;
-		updateDisplay();
-        setItems();
-        getItem();
-        
-        
-    }
+    launched_count++;
+    updateDisplay();
+    setItems();
+    getItem();          
+}
 
-   function setItems() {
-       var key = "phone";
-       var value = "black";
-       window.localStorage.setItem( key, value );
+function setItems() {
+    var key = "phone";
+    var value = "black";
+    window.localStorage.setItem( key, value );
        
-       key = "cable";
-       value = "white";
-       window.localStorage.setItem( key, value );
+    key = "cable";
+    value = "white";
+    window.localStorage.setItem( key, value );
        
-       key = "notepad";
-       value = "blue";
-       window.localStorage.setItem( key, value );
+    key = "notepad";
+    value = "blue";
+    window.localStorage.setItem( key, value );
        
-       key = "chair";
-       value = "blue";
-       window.localStorage.setItem( key, value );
+    key = "chair";
+    value = "blue";
+    window.localStorage.setItem( key, value );
        
-       key = "jacket";
-       value = "blue";
-       window.localStorage.setItem( key, value );
+    key = "jacket";
+    value = "blue";
+    window.localStorage.setItem( key, value );
    }
 
-    function getItem() {
-        var key = "phone";
-        var value = window.localStorage.getItem(key);
+function getItem() {
+    var key = "phone";
+    var value = window.localStorage.getItem(key);
+    var num = window.localStorage.length();
         
-        $("#item").text("Item: " + value);
-    }
+    $("#item").text("Item: " + value);
+    $("#num").text("Number of items: " + num);
+}
 
-    // Handle the pause event
-    //
-    function onPause() {
-		alert("paused now");
-		paused_count++;
-		updateDisplay();
+// Handle the pause event
+//
+function onPause() {
+    alert("paused now");
+    paused_count++;
+    updateDisplay();
         
         
-    }
+}
 	
-	function onResume() {
-		alert("resume now");
-		resumed_count++;
-		updateDisplay();
+function onResume() {
+    alert("resume now");
+    resumed_count++;
+    updateDisplay();
         
         
-    }
+}
