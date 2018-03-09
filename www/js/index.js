2354ec979d12f473318e9677b869afaf5b743d49
DeceptiveHedge <monj1_14@uni.worc.ac.uk>
2018-03-09 14:21:23 +0000

    fixed issues

--- www/js/index.js
+++ www/js/index.js
@@ -6,21 +6,29 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	// Use an HTML GET request to obtain data from an API
 	var xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        var ractive = new Ractive({
-    	el: 'container', <!-- where -->
-    	template: '#myTemplate', <!-- how -->
-    	data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
-	   });
+        if (this.readyState == 4 && this.status == 200) {
+            
+            // parse the resulting JSON into Javascript Data Object 
+	        // you can use a live parser to inspect the contents of the JSON
+	        // http://json.parser.online.fr/ 
+            var weather= JSON.parse(xmlhttp.responseText);
+            
+            //Define Ractive binding
+            var ractive = new Ractive({
+    	    el: 'container', <!-- where -->
+    	    template: '#myTemplate', <!-- how -->
+    	    data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
+	        });
         }
+      
     };
+               
 	xmlhttp.open("GET", feedURL, true);
 	xmlhttp.send();
 		
 		
-	// parse the resulting JSON into Javascript Data Object 
-	// you can use a live parser to inspect the contents of the JSON
-	// http://json.parser.online.fr/ 
-	var weather= JSON.parse(xmlhttp.responseText);
+	
+	
 	
 	
 	//Define Ractive binding
